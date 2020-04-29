157f46cd29c445ebf9dbbd99bdc494b999ad1fa7
Naotsugu Someya <chome.au.lait.github@gmail.com>
2020-04-29 18:45:09 +0900

    grpc->echo戻し

--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module github.com/echo-marche/hack-tech-tips-api
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/golang/protobuf v1.4.0
-	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
-	github.com/grpc-ecosystem/grpc-gateway v1.14.4 // indirect
 	github.com/jinzhu/gorm v1.9.12
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mwitkow/go-proto-validators v0.3.0
+	github.com/valyala/fasttemplate v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20200423211502-4bdfaf469ed5 // indirect
 	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
-	google.golang.org/genproto v0.0.0-20200424135956-bca184e23272
+	google.golang.org/genproto v0.0.0-20200424135956-bca184e23272 // indirect
 	google.golang.org/grpc v1.29.1
 	google.golang.org/protobuf v1.21.0
+	gopkg.in/yaml.v2 v2.2.3 // indirect
 )
 
 // for realize
--- main.go
+++ main.go
@@ -1,39 +1,22 @@
 package main
 
 import (
-	"log"
-	"net"
-
-	"github.com/echo-marche/hack-tech-tips-api/config"
 	"github.com/echo-marche/hack-tech-tips-api/infrastructure"
-	pb "github.com/echo-marche/hack-tech-tips-api/proto/pb/main_api"
-	"github.com/echo-marche/hack-tech-tips-api/servers"
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_validator "github.com/grpc-ecosystem/go-grpc-middleware/validator"
-	"google.golang.org/grpc"
 )
 
 func main() {
-	// DB connection
-	db := infrastructure.NewDb()
-	defer db.Close()
-
-	// init gRPC server
-	listenPort, err := net.Listen("tcp", ":"+config.GetEnv("API_PORT"))
-	if err != nil {
-		log.Fatalln(err)
+	// Presence API connection
+	presenceApiConn := infrastructure.NewPresenceApiConnector()
+	defer presenceApiConn.Close()
+	// Article API connection
+	articleApiConn := infrastructure.NewArticleApiConnector()
+	defer articleApiConn.Close()
+	// API settings
+	router := infrastructure.Router{
+		PresenceApiConn: presenceApiConn,
+		ArticleApiConn:  articleApiConn,
 	}
-	server := grpc.NewServer(
-		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
-			grpc_validator.StreamServerInterceptor(),
-		)),
-		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-			grpc_validator.UnaryServerInterceptor(),
-		)),
-	)
-	sampleServer := &servers.SampleServer{}
-	pb.RegisterMainApiServer(server, sampleServer)
-	if err := server.Serve(listenPort); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	router.InitRouter()
+	// Start API Server
+	router.Start()
 }
