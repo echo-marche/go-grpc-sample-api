2b5e420aaeccb9d31b13f91286c4afb258a321e7
Naotsugu Someya <chome.au.lait.github@gmail.com>
2020-05-17 09:35:36 +0900

    for k8s

--- config/api_server.go
+++ config/api_server.go
@@ -6,42 +6,42 @@ type ApiConfig struct {
 
 func InitPresenceApiConfig() ApiConfig {
 	return ApiConfig{
-		Url: GetEnv("PRESENCE_API_HOST") + ":" + GetEnv("PRESENCE_API_PORT"),
+		Url: GetEnv("PRESENCE_API_SERVICE_HOST") + ":" + GetEnv("PRESENCE_API_SERVICE_PORT"),
 	}
 }
 
 func InitArticleApiConfig() ApiConfig {
 	return ApiConfig{
-		Url: GetEnv("ARTICLE_API_HOST") + ":" + GetEnv("ARTICLE_API_PORT"),
+		Url: GetEnv("ARTICLE_API_SERVICE_HOST") + ":" + GetEnv("ARTICLE_API_SERVICE_PORT"),
 	}
 }
 
 func InitDepositApiConfig() ApiConfig {
 	return ApiConfig{
-		Url: GetEnv("DEPOSIT_API_HOST") + ":" + GetEnv("DEPOSIT_API_PORT"),
+		Url: GetEnv("DEPOSIT_API_SERVICE_HOST") + ":" + GetEnv("DEPOSIT_API_SERVICE_PORT"),
 	}
 }
 
 func InitNotificationApiConfig() ApiConfig {
 	return ApiConfig{
-		Url: GetEnv("NOTIFICATION_API_HOST") + ":" + GetEnv("NOTIFICATION_API_PORT"),
+		Url: GetEnv("NOTIFICATION_API_SERVICE_HOST") + ":" + GetEnv("NOTIFICATION_API_SERVICE_PORT"),
 	}
 }
 
 func InitPopularityApiConfig() ApiConfig {
 	return ApiConfig{
-		Url: GetEnv("POPULARITY_API_HOST") + ":" + GetEnv("POPULARITY_API_PORT"),
+		Url: GetEnv("POPULARITY_API_SERVICE_HOST") + ":" + GetEnv("POPULARITY_API_SERVICE_PORT"),
 	}
 }
 
 func InitInquiryApiConfig() ApiConfig {
 	return ApiConfig{
-		Url: GetEnv("INQUIRY_API_HOST") + ":" + GetEnv("INQUIRY_API_PORT"),
+		Url: GetEnv("INQUIRY_API_SERVICE_HOST") + ":" + GetEnv("INQUIRY_API_SERVICE_PORT"),
 	}
 }
 
 func InitSendmailApiConfig() ApiConfig {
 	return ApiConfig{
-		Url: GetEnv("SENDMAIL_API_HOST") + ":" + GetEnv("SENDMAIL_API_PORT"),
+		Url: GetEnv("SENDMAIL_API_SERVICE_HOST") + ":" + GetEnv("SENDMAIL_API_SERVICE_PORT"),
 	}
 }
